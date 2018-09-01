9fef5f39c638f976802dcd4f8bb1832cc424eff1
mwc <1819293933@qq.com>
2018-09-01 22:10:07 +0800

    添加注释

--- ab_jq.js
+++ ab_jq.js
@@ -10,8 +10,8 @@ let $ = (function(){
 			}
 			/*
 			*绑定事件
-			*{事件，函数}
-			* 事件首字母为大写为捕获阶段触发，小写为冒泡阶段触发
+			*参数{事件，函数}(object)
+			* 事件字母为大写为捕获阶段触发，小写为冒泡阶段触发
 			*/
 			behavior(evfn){
 				let b;
@@ -25,19 +25,20 @@ let $ = (function(){
 				return this;
 			}
 			// 总仓
-			// sDom是元素，fn是
+			// sDom是元素
 			GeneralWarehouse(sDom,fn){
+				//返回一个由小到大的dom元素数组
 				var collect = this.EnterGoods(sDom);
-                var commodity = this.Shipments(collect);
+				//将返回数组appendChild到各自的父级上
+                collect = this.Shipments(collect);
                 this.from.forEach(function(val){
-                    if(commodity instanceof Array){
-                        commodity.forEach(function(dom){
+                    if(collect instanceof Array){
+                        collect.forEach(function(dom){
+                        	//this.from的元素，和即将插入的dom
                         	fn(val,dom);
-                            // val.appendChild(dom);
                         })
                     }else{
-                    	fn(val,dom)
-                        // val.appendChild(commodity);
+                    	fn(val,collect)
                     }
                 })
 			}
@@ -45,30 +46,84 @@ let $ = (function(){
 			EnterGoods(sDom){
 				//元素数组,家属数组，包装后的数组
 				let els,family,collect = [];
-				if(sDom.indexOf("<")==-1){
-					[els,family]= this.turnHTML(sDom); 
-					els.forEach((val,i)=>{
-						var familyi = undefined;
-						val = this.Scalpel(val);
-						if(i)familyi = family[i-1];
-						this.Packing(familyi,this.machining(val),collect);
-					})      
-					return collect.reverse(); 
-				}else{
-
-				}
+				[els,family]= this.turnHTML(sDom); 
+				els.forEach((val,i)=>{
+					var familyi = undefined;
+					val = this.Scalpel(val);
+					if(i)familyi = family[i-1];
+					this.Packing(familyi,this.machining(val),collect);
+				})      
+				return collect.reverse(); //为了更加方便遍历
 			}
 			//原材料切割
+			//从字符串中提取出元素字符串element(Array)和关系relationship(Array)（+，>）
 			turnHTML(sDom){
 				let element = sDom.match(/[^>+]+/g);//截取元素
 				let relationship = sDom.match(/[>+]+/g);//截取家属关系
 				return [element,relationship];
 			}
 			//小刀切割，元素切割出来的数组
+			//元素字符串二次切割，返回的数组第一个元素是dom，后面是id，类，属性，文本等
 			Scalpel(sDom){
 				return sDom.match(/[#\.]?[^\s\.#\[\{]+=?[\w\/?]*[\]\}]?|[\[\{]?[^\]\}]+=?[\w\/?]*[\]\}]?/gi);
 			}
-			//出货
+			/*
+			*加工，字符串转为dom，并为其添加属性或文本
+			*参数：数组
+			*返回的是一个完整的dom
+			*/
+			machining(aA){
+				let dom = document.createElement(aA.shift());
+				aA.forEach(function(val){
+					switch(val.slice(0,1))
+					{
+						case '#'://id
+						dom.id = val.slice(1);
+						return;
+						case '.'://类
+						var cn = dom.className;
+						if(cn)cn+=' '
+						dom.className = cn+val.slice(1);
+						return;
+						case '['://属性
+						var attrs = val.slice(1,-1).split('=');
+						var attri = attrs.shift();
+						var sS = '';
+						attrs.forEach(function(val){
+							sS+=val;
+						})
+						dom.setAttribute(attri,sS);
+						return;
+						case '{'://文本
+						var text = val.slice(1,-1);
+						dom.appendChild(document.createTextNode(text));
+						return;
+					}
+				})
+				return dom;
+			}
+			//包装
+			//参数：关系val，dom元素elsi，dom数组collect
+			//兄弟则放在同一个数组里。
+			//collect里的元素+1下标是该元素的子元素，如果该元素是数组，则数组最后一个才是父元素
+			Packing(val,elsi,collect){
+				let brother;
+				//如果关系是+(兄弟)
+				if(val == '+'){
+					var pop = collect.pop();
+					if(pop instanceof Array){
+						brother = pop;
+					}else{
+						brother = [pop];
+					}
+					brother.push(elsi);
+					collect.push(brother);
+				}else{
+					collect.push(elsi);
+				}
+            }
+            //出货
+            //
 			Shipments(collect){
 				for(let i = 0;i<collect.length-1;i++){
 					var colli;
@@ -87,61 +142,13 @@ let $ = (function(){
 				}
 				return collect.slice(-1)[0];
 			}
-			//加工
-			machining(aA){
-				let dom = document.createElement(aA.shift());
-				aA.forEach(function(val){
-					switch(val.slice(0,1))
-					{
-						case '#':
-						dom.id = val.slice(1);
-						return;
-						case '.':
-						var cn = dom.className;
-						if(cn)cn+=' '
-						dom.className = cn+val.slice(1);
-						return;
-						case '[':
-						var attrs = val.slice(1,-1).split('=');
-						var attri = attrs.shift();
-						var sS = '';
-						attrs.forEach(function(val){
-							sS+=val;
-						})
-						dom.setAttribute(attri,sS);
-						return;
-						case '{':
-						var text = val.slice(1,-1);
-						dom.appendChild(document.createTextNode(text));
-						return;
-					}
-				})
-				return dom;
-			}
-			//包装
-			Packing(val,elsi,collect){
-				let brother;
-				if(val == '+'){
-					var pop = collect.pop();
-					if(pop instanceof Array){
-						brother = pop;
-					}else{
-						brother = [pop];
-					}
-					brother.push(elsi);
-					collect.push(brother);
-				}else{
-					collect.push(elsi);
-				}
-            }
-// $('body').append('a[href=http://mwc.ac.cn/]{没有东西的网址}+h1{大佬}>b{好}[style=color:#ee6611]+span{乱敲}[style=font-size:16px;display:block;text-align:center]>b{span下的b}')
 			append(sDom){
-				//总仓调控
+				//val为this.from的元素，dom是即将要被插入的dom碎片
 				this.GeneralWarehouse(sDom,function(val,dom){
 					val.appendChild(dom);
 				});
 				return this;
-			},
+			}
 			/*
 			*改变背景颜色
 			*cols：颜色，up上限,lower下限，ch渐变度
@@ -176,3 +183,5 @@ let $ = (function(){
 	}
 	return init;
 })();
+// $('body').append('a[href=http://mwc.ac.cn/]{没有东西的网址}+h1{大佬}>b{好}[style=color:#ee6611]+span{乱敲}[style=font-size:16px;display:block;text-align:center]>b{span下的b}')
+
