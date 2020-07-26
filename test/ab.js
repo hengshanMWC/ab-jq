115caf601ac7cf6848de7cf1dd4bc408608d86bd
mwc <1819293933@qq.com>
2020-07-26 20:28:04 +0800

    feat: 第一版

---
+++ test/ab.js
@@ -0,0 +1,23 @@
+function Drop(e){
+	e.preventDefault();
+  var data=e.dataTransfer.getData("Text");
+  const element = document.getElementById(data)
+  if (element === e.target) return
+	e.target.appendChild(element);
+}
+function Dragover(e){
+	e.preventDefault();
+}
+function Dragstart(e){
+	e.dataTransfer.setData("Text",e.target.id);
+}
+$('.a1').behavior({
+		"Drop":Drop,
+		"Dragover":Dragover
+});
+$('#tu').behavior({"dragstart":Dragstart});
+// $('body').append('a[href=http://mwc.ac.cn/]{没有东西的网址}+h1{大佬}>b{好}[style=color:#ee6611]+span{乱敲}[style=font-size:16px;display:block;text-align:center]>b{span下的b}')
+// $('body').append('a[href=http://mwc.ac.cn/]{没有东西的网址}+h1{大佬}>b{好}[style=color:#ee6611]+span{乱敲}[style=font-size:16px;display:block;text-align:center]>b{span下的b}')
+// $('body').append('a[href=http://mwc.ac.cn/]{没有东西的网址}+h1{大佬}>b{好}[style=color:#ee6611]+span{乱敲}[style=font-size:16px;display:block;text-align:center]>b{span下的b}')
+// $('h1').addClass(['b1','asd','a12','a1'])
+// $('h1').addClass('b1')
